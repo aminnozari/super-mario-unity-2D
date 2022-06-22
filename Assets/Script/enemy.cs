853b0cda537222d9f9c98362b4dad83eeecf49c0
Amin Nozari <41434431+aminnozari@users.noreply.github.com>
2022-06-22 18:47:59 +0430

    new version!!!

---
+++ Assets/Script/enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class enemy : MonoBehaviour
+{
+    public float speed = 8;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
+    }
+    void OnCollisionEnter2D(Collision2D target)
+    {
+        if(target.gameObject.name.StartsWith("mario"))
+        { 
+            Destroy(target.gameObject);
+            Debug.Log("game over");
+        }
+    }
+}
---
+++ Assets/Script/gharchkhor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gharchkhor : MonoBehaviour
+{
+    private Rigidbody2D mario;
+    public float speed = 6;
+    public float jump = 4;
+    public int score;
+    public GameObject coin;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mario = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float s = Input.GetAxisRaw("Horizontal");
+        mario.velocity = new Vector2(s*speed,mario.velocity.y);
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+            mario.velocity += Vector2.up*jump;
+    }
+    void  OnCollisionEnter2D(Collision2D coin)
+    {
+        if (coin.gameObject.name.StartsWith("coin"))
+        {
+            Destroy (coin.gameObject);
+            score +=1;
+            Debug.Log(score);
+        }
+    }
+}
